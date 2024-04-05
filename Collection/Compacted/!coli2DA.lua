82754324a5363f651fde0c9e695ed61be81d5635
DuckAfire <155199080+DuckAfire@users.noreply.github.com>
2024-04-04 23:09:44 -0300

    Updated comments structure

--- Collection/Compacted/!coli2DA.lua
+++ Collection/Compacted/!coli2DA.lua
@@ -1,11 +1,36 @@
+----------------------------------------------------------------------------------
+-- MIT License
+
+-- Copyright (c) 2024 DuckAfire
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+----------------------------------------------------------------------------------
+
 -- [COMPACTED] Copy and paste the code in your cart [v: 2.7]
 
-local DAMIT,DA="github.com/DuckAfire/TinyLibrary/blob/main/LICENSE",{}
+local DA={}
+local TINY_LIBRARY_LICENSE="github.com/DuckAfire/TinyLibrary/blob/main/LICENSE"
 
 do local _T,_M=type,math local function ERROR(p,f,n) error('[coli2DA] The parameter "Type" is invalid,try "'..p..'". In function "coli.'..f..'", argument #'..n..'.') end DA.newBody=function(t,x,y,wr,h) local b={x=x or 0,y=y or 0} if t=="rect" then b.width,b.height=wr or 8,h or 8 elseif t=="circ" then b.radius=wr or 4 elseif t~="simp" then ERROR('rect","circ" or "simp',"newBody","1") end return b end local function _C(p,t) local I,b,l=1,{{},{}},0 for i=1,#t do local a,c,r=t[i]=="circ" and 1 or t[i]=="simp" and 2 or 0,p[I],0 if _T(c)=="table" then b[i]=DA.newBody(t[i],c.x or c[1],c.y or c[2],c.width or cur.radius or c[3],c.height or c[4]) l=l+1 else r,l,b[i]=3,l+4-a,DA.newBody( t[i],p[I],p[I + 1],p[I + 2],p[I + 3] ) end if r==3 then I=5-a else I=2 end end return b[1],b[2],p,l+1 end DA.distance=function(...) local a,b=_C({...},{"simp","simp"}) return _M.sqrt((a.x-b.x)^2+(a.y-b.y)^2) end DA.mapAlign=function(...) local P={...} local t=(_T( P[1] )=="table" or (_T( P[1] )~="table" and #P>=4)) and "rect" or "simp" if _T(P[1])~= "table" and #P<2 then error( '[coli2DA] Insufficient parameters for this call format. In function "coli.mapAlign", argument #(quantity).' ) end local o,_,p,l=_C(P,{t}) local x,y=o.x,o.y if p[l] then local w,h=o.width or 8,o.height or 8 x,y=x+w//2,y+h//2 end return (x//8)*8,(y//8)*8 end DA.tile=function(...) local o,_,p,l=_C({...},{"rect"}) local w,h,f,x,y,a,b,c,d=o.width,o.height,p[l+1] or 0,p[l+2] or 0,p[l+3] or 0 if p[l]=="top" then a,b,c,d=0,-1,w-1,-1 elseif p[l]=="below" then a,b,c,d=0,h,w-1,h elseif p[l]=="left" then a,b,c,d=-1,0,-1,h-1 elseif p[l]=="right" then a,b,c,d=w,0,w,h-1 else ERROR('top","below","left" or "right',"tile","(last)") end return fget(mget((o.x+a)//8+x,(o.y+b)//8+y),f) and fget(mget((o.x+c)//8+x,(o.y+d)//8+y),f) end DA.tileCross=function(...) local o,_,p,l=_C({...},{"rect"}) local f=_T(p[l+1])=="table" and p[l+1] or {} for i=1,4 do if not f[i] or _T(f[i])~="number" then f[i]=_T(p[l+1])=="number" and p[l+1] or 0 end end local a,b,v,t=p[l+2],p[l+3],{},{[0]="top","below","left","right"} for i=0,3 do v[i]=DA.tile(o,t[i],f[i+1],a,b) end return p[l] and {top=v[0],below=v[1],left=v[2],right=v[3]} or v end DA.box360=function(x,y,s) local tb,lr if s.top then tb=y and ((y<0 and s.top) or (y>0 and s.below)) or false lr=x and ((x<0 and s.left) or (x>0 and s.right)) or false else tb=y and ((y < 0 and s[0]) or (y>0 and s[1])) or false lr=x and ((x < 0 and s[2]) or (x>0 and s[3])) or false end return tb or lr end DA.touch=function(a,b,A,B,d) local c,d={},d or {} c.x,c.y=mouse() c.width,c.height=d.width and _M.abs(d.width) or d[1] and _M.abs(d[1]) or 1,d.height and _M.abs(d.height) or d[2] and _M.abs(d[2]) or 1 return c.x+c.width-1>=a and c.x<=A and c.y+c.height-1>=b and c.y<=B end DA.impactPixel=function(...) local t={...} if t[#t]~="rect" and t[#t]~="circ" then ERROR('rect" or "circ',"impactPixel","(last)") end local a,b,p,l=_C(t,{t[#t],t[#t]}) if p[l]=="rect" then local A,B={x=a.x+a.width/2,y=a.y+a.height/2,radius=(a.width+a.height)/2},{x=b.x+b.width/2,y=b.y+b.height/2,radius=(b.width+b.height)/2} return DA.impactPixel(A,B,"circ") elseif p[l]=="circ" then local x,y=(a.x*b.radius)+(b.x*a.radius),(a.y*b.radius)+(b.y*a.radius) local tr=(a.radius + b.radius) return x/tr,y/tr end end DA.rectangle=function(...) local t,i,A={...} i=_T(t[#t])=="boolean" and t[#t] or false A=i and "simp" or "rect" local a,b=_C(t,{A,"rect"}) if i then a.width,a.height=1,1 end return a.x+a.width-1>=b.x and a.x<=b.x+b.width-1 and a.y+a.height-1>=b.y and a.y<=b.y+b.height-1 end DA.circle=function(...) local t,i,A={...} i=_T(t[#t])=="boolean" and t[#t] or false A=i and "simp" or "circ" local a,b=_C(t,{A,"circ"}) local tr=i and b.radius or a.radius + b.radius return (a.x-b.x)^2+(a.y-b.y)^2<=tr^2 end DA.shapesMix=function(...) local C,R,r,s,x,y=_C({...},{"circ","rect"}) r=C.radius s=DA.newBody("rect",C.x-r,C.y-r,r*2,r*2) x,y=DA.impactPixel(R,s,"rect") return DA.rectangle({x,y},R,true) or DA.circle({x,y},C,true) end end
 
-local coli=DA
+local coli=DA -- you can customize this reference or not use it.
+DA=nil
 
--- NOTES:
+-- NOTE:
 ---- In function "impactPixel" (using "rect"),if any of the bodies is a rectangle with a large difference in size between its pairs of sides (e.g.,5x30),the returned result may is incorrect.
-
--- Collection/Compacted/!longBit.lua
+++ Collection/Compacted/!longBit.lua
@@ -1,7 +1,33 @@
+----------------------------------------------------------------------------------
+-- MIT License
+
+-- Copyright (c) 2024 DuckAfire
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+----------------------------------------------------------------------------------
+
 -- [COMPACTED or NOT COMPACTED] Copy and paste the code in your cart [v: 1.0]
 
-local DAMIT,DA="github.com/DuckAfire/TinyLibrary/blob/main/LICENSE",{}
+local DA={}
+local TINY_LIBRARY_LICENSE="github.com/DuckAfire/TinyLibrary/blob/main/LICENSE"
 
 do _G.__LONGBIT_CLASSES={} local _TS,_TN,_T,_SS = tostring,tonumber,type,string.sub local function _C(d) assert(_G.__LONGBIT_CLASSES[1]~=nil,"[longBit] pmem classes not defined.") for i=1,#_G.__LONGBIT_CLASSES do if d==_G.__LONGBIT_CLASSES[i] then return i-1 end end error('[longBit] Undefined class: "'..d..'"') end DA.setClass=function(...) _G.__LONGBIT_CLASSES={...} end DA.setMem=function(n,P,C,L) local p,l,v,b,f=P+1,L or 1 if _T(n)=="boolean" then v=n and 1 or 0 else v=_TS(n) if l>1 then for i=1,l do v=(#v<l) and "0"..v or v end end end local function c(a,z) return _SS(_TS(pmem(_C(C))),a,z) end b,f= c(1,p-1),c(p+l) pmem(_C(C),_TN(b..v..f)) end DA.boot=function(v,z,a) for i=a or 0,z or #v-1,(A~=nil and A>Z) and -1 or 1 do pmem(i,_TN(v[i+1])) end end DA.clear=function(Z,A) for i=A or 0,Z or 255,(A~=nil and A>Z) and -1 or 1 do pmem(i,0) end end DA.getNum=function(P,c,l) local p=P+1 return _TN(_SS(_TS(pmem(_C(c))),p,p+((l or 1)-1))) end DA.getBool=function(p,c,e) return DA.getNum(p,c)==(e or 1) end DA.getClass=function(i) return _G.__LONGBIT_CLASSES[i] end end
 
-local lbit=DA
+local lbit=DA -- you can customize this reference or not use it.
+DA=nil
